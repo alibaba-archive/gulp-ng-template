7db783ccefd9c1e94ed55378b0193745aa19c1fc
Halif Bakri <halif.bakri@gmail.com>
2015-02-24 15:43:49 +0100

    Added a prefix option. This will allow you to add a prefix to the ngtemplate key. I've added test for it run gulp subfolders to run the tests.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = function(options) {
   var standalone = options.standalone ? ', []' : '';
   var moduleName = options.moduleName || 'ngTemplates';
   var filePath = options.filePath || 'templates.js';
+  var prefix = options.prefix || '';
   var joinedContent = '';
   var headerTpl = '\'use strict\';\n\nangular.module(\'<%= module %>\'<%= standalone %>).run([\'$templateCache\', function($templateCache) {\n\n';
   var contentTpl = '  $templateCache.put(\'<%= name %>\', \'<%= content %>\');\n\n';
@@ -43,7 +44,8 @@ module.exports = function(options) {
     if (file.isNull()) return next();
     if (file.isStream()) return this.emit('error', new gutil.PluginError(packageName,  'Streaming not supported'));
 
-    var name = path.relative(file.base, file.path);
+    var name = prefix;
+	name += path.relative(file.base, file.path);
     joinedContent += gutil.template(contentTpl, {
       name: normalizeName(name),
       content: normalizeContent(file.contents),
