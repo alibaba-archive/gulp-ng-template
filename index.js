ef538b9026ea93c8c1539dda6717624cf7a670e1
zensh <admin@zensh.com>
2015-03-18 21:51:10 +0800

    improve code

--- index.js
+++ index.js
@@ -15,32 +15,17 @@ var packageName = require('./package.json').name;
 module.exports = function(options) {
   options = options || {};
 
-  var standalone = options.standalone ? ', []' : '';
-  var moduleName = options.moduleName || 'ngTemplates';
-  var filePath = options.filePath || 'templates.js';
+  var joinedContent = '';
   var wrap = options.wrap !== false;
   var prefix = options.prefix || '';
-  var joinedContent = '';
+  var filePath = options.filePath || 'templates.js';
+  var standalone = options.standalone ? ', []' : '';
+  var moduleName = options.moduleName || 'ngTemplates';
   var headerTpl = '\'use strict\';\n\nangular.module(\'<%= module %>\'<%= standalone %>).run([\'$templateCache\', function($templateCache) {\n\n';
   var contentTpl = '  $templateCache.put(\'<%= name %>\', \'<%= content %>\');\n\n';
-
   var joinedHeader = gutil.template(headerTpl, {module: moduleName, standalone: standalone, file: ''});
 
-  var joinedFile = new gutil.File({
-    cwd: __dirname,
-    base: __dirname,
-    path: path.join(__dirname, filePath)
-  });
-
-  function normalizeName(name) {
-    return name.replace(/\\/g, '/');
-  }
-
-  function normalizeContent(content) {
-    return content.toString('utf8').replace(/\'/g, '\\\'').replace(/\r?\n/g, '\\n');
-  }
-
-  return through.obj(function (file, encoding, next) {
+  return through.obj(function(file, encoding, next) {
     if (file.isNull()) return next();
     if (file.isStream()) return this.emit('error', new gutil.PluginError(packageName,  'Streaming not supported'));
 
@@ -52,13 +37,18 @@ module.exports = function(options) {
       file: ''
     });
     next();
-  }, function () {
-    var content = joinedHeader + joinedContent + '}]);';
-    if (wrap) {
-      content = ';(function(){\n\n' + content + '\n\n})();';
-    }
-    joinedFile.contents = new Buffer(content);
-    this.push(joinedFile);
-    this.push(null);
+  }, function(callback) {
+    var contents = joinedHeader + joinedContent + '}]);';
+    if (wrap) contents = ';(function(){\n\n' + contents + '\n\n})();';
+    this.push(new gutil.File({base: '',  path: filePath, contents: new Buffer(contents)}));
+    callback();
   });
 };
+
+function normalizeName(name) {
+  return name.replace(/\\/g, '/');
+}
+
+function normalizeContent(content) {
+  return content.toString().replace(/\'/g, '\\\'').replace(/\r?\n/g, '\\n');
+}
