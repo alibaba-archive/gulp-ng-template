c73f8af481b6b74ac5da762e080875849981665c
zensh <admin@zensh.com>
2018-12-27 19:55:57 +0800

    update for #8

--- index.js
+++ index.js
@@ -6,41 +6,47 @@
  * Licensed under the MIT license.
  */
 
-var path = require('path')
-var gutil = require('gulp-util')
-var through = require('through2')
-var packageName = require('./package.json').name
+const path = require('path')
+const through = require('through2')
+const Vinyl = require('vinyl')
+const PluginError = require('plugin-error')
+const template = require('lodash.template')
+const packageName = require('./package.json').name
 
 module.exports = function (options) {
   options = options || {}
 
-  var joinedContent = ''
-  var useStrict = options.useStrict !== false
-  var wrap = options.wrap !== false
-  var prefix = options.prefix || ''
-  var filePath = options.filePath || 'templates.js'
-  var standalone = options.standalone ? ', []' : ''
-  var moduleName = options.moduleName || 'ngTemplates'
-  var headerTpl = (useStrict ? "'use strict';\n\n" : '') + "angular.module('<%= module %>'<%= standalone %>).run(['$templateCache', function($templateCache) {\n\n"
-  var contentTpl = "  $templateCache.put('<%= name %>', '<%= content %>');\n\n"
-  var joinedHeader = gutil.template(headerTpl, {module: moduleName, standalone: standalone, file: ''})
+  let joinedContent = ''
+  const useStrict = options.useStrict !== false
+  const wrap = options.wrap !== false
+  const prefix = options.prefix || ''
+  const filePath = options.filePath || 'templates.js'
+  const standalone = options.standalone ? ', []' : ''
+  const moduleName = options.moduleName || 'ngTemplates'
+  const headerTpl = (useStrict ? "'use strict';\n\n" : '') + "angular.module('<%= module %>'<%= standalone %>).run(['$templateCache', function($templateCache) {\n\n"
+  const contentTpl = "  $templateCache.put('<%= name %>', '<%= content %>');\n\n"
+  const joinedHeader = template(headerTpl, { module: moduleName, standalone: standalone, file: '' })
 
-  return through.obj(function (file, encoding, next) {
-    if (file.isNull()) return next()
-    if (file.isStream()) return this.emit('error', new gutil.PluginError(packageName, 'Streaming not supported'))
+  return through.obj(function (file, _encoding, next) {
+    if (file.isNull()) {
+      return next()
+    }
+    if (file.isStream()) {
+      return this.emit('error', new PluginError(packageName, 'Streaming not supported'))
+    }
 
-    var name = prefix
+    let name = prefix
     name += path.relative(file.base, file.path)
-    joinedContent += gutil.template(contentTpl, {
+    joinedContent += template(contentTpl, {
       name: normalizeName(name),
       content: normalizeContent(file.contents),
       file: ''
     })
     next()
   }, function (callback) {
-    var contents = joinedHeader + joinedContent + '}]);'
+    let contents = joinedHeader + joinedContent + '}]);'
     if (wrap) contents = ';(function(){\n\n' + contents + '\n\n})();'
-    this.push(new gutil.File({base: '', path: filePath, contents: new Buffer(contents)}))
+    this.push(new Vinyl({base: null, path: filePath, contents: Buffer.from(contents)}))
     callback()
   })
 }
